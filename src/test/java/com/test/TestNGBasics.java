745288b786a765ff8d20a1e7d3596024b37a940b
Ajit D <desai.ajit@gmail.com>
2020-08-24 14:59:40 -0400

    first commit

---
+++ src/test/java/com/test/TestNGBasics.java
@@ -0,0 +1,67 @@
+package com.test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNGBasics {
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		System.out.println("setUp the initial config");
+		System.setProperty("webdriver.chrome.driver", "D:\\EclipseInstallFolder\\Softwares\\chromedriver.exe");
+		
+				
+	}
+	@BeforeTest
+	public void launchBrowser() {
+		System.out.println("launch Browser");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	@BeforeClass
+	public void enterURL() {
+		System.out.println("enter URL");
+		driver.get("https://www.google.com/");
+	}
+	@BeforeMethod
+	public void login() throws InterruptedException {
+		System.out.println("enter login details");
+		driver.findElement(By.name("q")).sendKeys("Selenium");
+		Thread.sleep(2000);
+	}	
+	@Test
+	public void validateTitle() {
+		String title = driver.getTitle();
+		System.out.println("Check Title of the page");
+		Assert.assertEquals(title, "Google");
+	}	
+	@AfterMethod
+	public void logout() {
+		System.out.println("Logout");
+	}	
+	@AfterClass
+	public void ClearCoockies() {
+		System.out.println("Clear Coockies");
+		driver.manage().deleteAllCookies();
+	}
+	@AfterTest
+	public void closeBrowser() {
+		System.out.println("Close the browser");
+		driver.quit();
+	}
+	
+
+}
